9f2f9906b88d08b196278012ba11587cec2dc773
Benjy Marks <benjy.marks@sydney.edu.au>
2021-10-01 14:35:31 +1000

    serve all files on request

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const app = require('express')();
+var express = require('express');
+const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
