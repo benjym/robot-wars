141b91d0cdb0a488f12ccc882cd0fc61e7c4dca6
Benjy Marks <benjy.marks@sydney.edu.au>
2021-10-02 18:50:13 +1000

    minor updates

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use('/', express.static(__dirname + '/')); // serve data files
 
