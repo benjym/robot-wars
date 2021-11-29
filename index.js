e57c81510409410819bf7d09e54ca7cbc46b3af7
Benjamin Marks <benjy.marks@sydney.edu.au>
2021-11-29 17:55:02 +1100

    forked front end loader to its own page, generalised socket emits a bit better, added tokens

--- index.js
+++ index.js
@@ -1,22 +1,47 @@
-var express = require('express');
+const express = require("express");
+const cors = require('cors');
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
 const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const port = process.env.PORT || 5000;
+app.use(cors());
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
+var BBG_ID = 0;
+
+const port = process.env.PORT || 3000;
 
 app.use('/', express.static(__dirname + '/')); // serve data files
 
-// app.get('/', (req, res) => {
-//   res.sendFile(__dirname + '/index.html');
-// });
-//Adding Minor Change
+io.on('connection', (socket) => { // when a user connects
+  console.log('a user connected');
 
-io.on('connection', (socket) => {
-  socket.on('FEL_A_left_wheels', value => {
-    console.log('left wheel should be set to ' + value);
+  socket.on('i-am-the-BBG', id) => {
+      console.log('The BBG connected with socket id ' + id)
+      BBG_ID = id;
+  }
+
+  socket.on('front-end-loader', (actuator, value, team, token ) => {
+    console.log(actuator + ' should be set to ' + value + ' for team' + team);
+    socket.to(BBG_ID).emit('front-end-loader', (actuator, value, team, token ));
+  });
+
+  socket.on('dump-truck', (actuator, value, team, token ) => {
+    console.log(actuator + ' should be set to ' + value + ' for team' + team);
+    socket.to(BBG_ID).emit('dump-truck', (actuator, value, team, token ));
+  });
+
+  socket.on('excavator', (actuator, value, team, token ) => {
+    console.log(actuator + ' should be set to ' + value + ' for team' + team);
+    socket.to(BBG_ID).emit('excavator', (actuator, value, team, token ));
+  });
+
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
   });
 });
 
-http.listen(port, () => {
+httpServer.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
