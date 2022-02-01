ef109d14b4e6e59c7e05ff46b562eb16e61753a3
Benjy Marks <benjy.marks@sydney.edu.au>
2022-02-01 17:48:38 +1100

    tidied up arduino code. added basic keyboard commands for simple controls

--- arduino.js
+++ arduino.js
@@ -1,7 +1,7 @@
 const { io } = require("socket.io-client");
 const { Board, Leds } = require("johnny-five");
 const board = new Board();
-var leds, forward_pin, back_pin;
+var leds, forward_pin, back_pin, start_pin;
 
 var server = 'https://robot-wars-usyd.herokuapp.com/'; // production server
 // var server = 'http://localhost:3000'; // testing only
@@ -23,43 +23,35 @@ var start_pins = {
 }
 
 // 6 pin controller order:
-//   1. right wheels back
-//   2. right wheels forward
-//   3. bucket down
-//   4. bucket up
-//   5. left wheels forward
-//   6. left wheels back
+//   1. left wheels forward
+//   2. left wheels back
+//   3. right weels back
+//   4. right weels forward
+//   5. bucket up
+//   6. bucket down
+
+// OFF COLOUR WIRE IS PIN 1
 
 socket.on("connect", () => {
   console.log('I am the BBG. My socket ID is ' + socket.id); // "G5p5..."
-  socket.emit('i-am-alive', socket.id, 'BBG');
-
+  // socket.emit('i-am-alive', socket.id, 'BBG');
+  socket.emit('i-am-alive', 'BBG', socket.id);
   socket.on('front-end-loader', (actuator, value, team) => {
     console.log(actuator + ' should be set to ' + value + ' for team ' + team);
+    if ( team === 'a' ) { start_pin = start_pins['FEL_A'] }
+    else { start_pin = start_pins['FEL_B'] }
 
-    if      ( actuator === 'left_wheels'  & team === 'a' ) {
-        forward_pin = leds[start_pins['FEL_A'] + 4];
-        back_pin    = leds[start_pins['FEL_A'] + 5];
+    if      ( actuator === 'left_wheels' ) {
+        forward_pin = leds[start_pin + 0];
+        back_pin    = leds[start_pin + 1];
     }
-    else if ( actuator === 'right_wheels' & team === 'a' ) {
-        forward_pin = leds[start_pins['FEL_A'] + 1];
-        back_pin    = leds[start_pins['FEL_A']];
+    else if ( actuator === 'right_wheels' ) {
+        forward_pin = leds[start_pin + 3];
+        back_pin    = leds[start_pin + 2];
     }
-    else if ( actuator === 'bucket'       & team === 'a' ) {
-        forward_pin = leds[start_pins['FEL_A'] + 3];
-        back_pin    = leds[start_pins['FEL_A'] + 2];
-    }
-    else if  ( actuator === 'left_wheels'  & team === 'b' ) {
-        forward_pin = leds[start_pins['FEL_B'] + 4];
-        back_pin    = leds[start_pins['FEL_B'] + 5];
-    }
-    else if ( actuator === 'right_wheels' & team === 'b' ) {
-        forward_pin = leds[start_pins['FEL_B'] + 1];
-        back_pin    = leds[start_pins['FEL_B']];
-    }
-    else if ( actuator === 'bucket'       & team === 'b' ) {
-        forward_pin = leds[start_pins['FEL_B'] + 3];
-        back_pin    = leds[start_pins['FEL_B'] + 2];
+    else if ( actuator === 'bucket' ) {
+        forward_pin = leds[start_pin + 5];
+        back_pin    = leds[start_pin + 4];
     }
 
     v = parseInt(value); // just to be sure
@@ -78,32 +70,21 @@ socket.on("connect", () => {
   });
 
   socket.on('dump-truck', (actuator, value, team) => {
-    console.log(actuator + ' should be set to ' + value + ' for team ' + team);
+      if ( team === 'a' ) { start_pin = start_pins['DMP_A'] }
+      else { start_pin = start_pins['DMP_B']}
 
-    if      ( actuator === 'left_wheels'  & team === 'a' ) {
-        forward_pin = leds[start_pins['DMP_A'] + 4];
-        back_pin    = leds[start_pins['DMP_A'] + 5];
-    }
-    else if ( actuator === 'right_wheels' & team === 'a' ) {
-        forward_pin = leds[start_pins['DMP_A'] + 1];
-        back_pin    = leds[start_pins['DMP_A']];
-    }
-    else if ( actuator === 'bucket'       & team === 'a' ) {
-        forward_pin = leds[start_pins['DMP_A'] + 3];
-        back_pin    = leds[start_pins['DMP_A'] + 2];
-    }
-    else if  ( actuator === 'left_wheels'  & team === 'b' ) {
-        forward_pin = leds[start_pins['DMP_B'] + 4];
-        back_pin    = leds[start_pins['DMP_B'] + 5];
-    }
-    else if ( actuator === 'right_wheels' & team === 'b' ) {
-        forward_pin = leds[start_pins['DMP_B'] + 1];
-        back_pin    = leds[start_pins['DMP_B']];
-    }
-    else if ( actuator === 'bucket'       & team === 'b' ) {
-        forward_pin = leds[start_pins['DMP_B'] + 3];
-        back_pin    = leds[start_pins['DMP_B'] + 2];
-    }
+      if      ( actuator === 'left_wheels' ) {
+          forward_pin = leds[start_pin + 0];
+          back_pin    = leds[start_pin + 1];
+      }
+      else if ( actuator === 'right_wheels' ) {
+          forward_pin = leds[start_pin + 3];
+          back_pin    = leds[start_pin + 2];
+      }
+      else if ( actuator === 'bucket' ) {
+          forward_pin = leds[start_pin + 5];
+          back_pin    = leds[start_pin + 4];
+      }
 
     v = parseInt(value); // just to be sure
     if ( v === 1 ) {
@@ -122,55 +103,32 @@ socket.on("connect", () => {
 
   socket.on('excavator', (actuator, value, team) => {
     console.log(actuator + ' should be set to ' + value + ' for team ' + team);
+    if ( team === 'a' ) { start_pin = start_pins['EXC_A'] }
+    else { start_pin = start_pins['EXC_B']}
 
-    if      ( actuator === 'left_wheels'  & team === 'a' ) {
-        forward_pin = leds[start_pins['EXC_A']];
-        back_pin    = leds[start_pins['EXC_A'] + 1];
+    if      ( actuator === 'left_wheels' ) {
+        forward_pin = leds[start_pin + 0];
+        back_pin    = leds[start_pin + 1];
     }
-    else if ( actuator === 'right_wheels' & team === 'a' ) {
-        forward_pin = leds[start_pins['EXC_A'] + 2];
-        back_pin    = leds[start_pins['EXC_A'] + 3];
+    else if ( actuator === 'right_wheels' ) {
+        forward_pin = leds[start_pin + 3];
+        back_pin    = leds[start_pin + 2];
     }
-    else if ( actuator === 'bucket'       & team === 'a' ) {
-        forward_pin = leds[start_pins['EXC_A'] + 4];
-        back_pin    = leds[start_pins['EXC_A'] + 5];
+    else if ( actuator === 'bucket' ) {
+        forward_pin = leds[start_pin + 5];
+        back_pin    = leds[start_pin + 4];
     }
-    else if ( actuator === 'arm_1'        & team === 'a' ) {
-        forward_pin = leds[start_pins['EXC_A'] + 6];
-        back_pin    = leds[start_pins['EXC_A'] + 7];
+    else if ( actuator === 'arm_1' ) {
+        forward_pin = leds[start_pin + 6];
+        back_pin    = leds[start_pin + 7];
     }
-    else if ( actuator === 'arm_2'        & team === 'a' ) {
-        forward_pin = leds[start_pins['EXC_A'] + 8];
-        back_pin    = leds[start_pins['EXC_A'] + 9];
+    else if ( actuator === 'arm_2' ) {
+        forward_pin = leds[start_pin + 8];
+        back_pin    = leds[start_pin + 9];
     }
-    else if ( actuator === 'slew'         & team === 'a' ) {
-        forward_pin = leds[start_pins['EXC_A'] + 10];
-        back_pin    = leds[start_pins['EXC_A'] + 11];
-    }
-
-    else if ( actuator === 'left_wheels'  & team === 'b' ) {
-        forward_pin = leds[start_pins['EXC_B']];
-        back_pin    = leds[start_pins['EXC_B'] + 1];
-    }
-    else if ( actuator === 'right_wheels' & team === 'b' ) {
-        forward_pin = leds[start_pins['EXC_B'] + 2];
-        back_pin    = leds[start_pins['EXC_B'] + 3];
-    }
-    else if ( actuator === 'bucket'       & team === 'b' ) {
-        forward_pin = leds[start_pins['EXC_B'] + 4];
-        back_pin    = leds[start_pins['EXC_B'] + 5];
-    }
-    else if ( actuator === 'arm_1'        & team === 'b' ) {
-        forward_pin = leds[start_pins['EXC_B'] + 6];
-        back_pin    = leds[start_pins['EXC_B'] + 7];
-    }
-    else if ( actuator === 'arm_2'        & team === 'b' ) {
-        forward_pin = leds[start_pins['EXC_B'] + 8];
-        back_pin    = leds[start_pins['EXC_B'] + 9];
-    }
-    else if ( actuator === 'slew'         & team === 'b' ) {
-        forward_pin = leds[start_pins['EXC_B'] + 10];
-        back_pin    = leds[start_pins['EXC_B'] + 11];
+    else if ( actuator === 'slew' ) {
+        forward_pin = leds[start_pin + 10];
+        back_pin    = leds[start_pin + 11];
     }
 
     v = parseInt(value); // just to be sure
