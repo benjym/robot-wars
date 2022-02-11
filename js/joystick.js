1ce0154f671524f4531bf15d1b92ec2ececb17a6
Benjy Marks <benjy.marks@sydney.edu.au>
2022-02-12 08:23:11 +1100

    and another attempt

--- js/joystick.js
+++ js/joystick.js
@@ -3,6 +3,7 @@ export function add_joystick(container) {
     var track = document.createElement('div');
 
     thumb.setAttribute('class','joystick-thumb');
+    thumb.setAttribute('id', Math.random())
     track.setAttribute('class','joystick-track');
 
     container.appendChild(track);
@@ -11,41 +12,35 @@ export function add_joystick(container) {
     var active = false;
     var currentY;
     var initialY;
-    // var xOffset = 0;
-    // var yOffset = 0;
     var val = 0;
-    var finger;
 
-    var benjyOffset = container.clientHeight/2 - 40;
-    var maxDisplacement = container.clientHeight/2.;
+    var benjyOffset = container.clientHeight/2 - 40; // to get the thumb in the middle of the track
+    var maxDisplacement = container.clientHeight/2.; // half-length of track
 
-    setTranslate(0, thumb);
+    setTranslate(0, thumb); // put the thumb in the middle
+
+    function update_orientation() {
+        var afterOrientationChange = function() { // NOTE: need an extra step to get the new dimensions _AFTER_ rotation
+            benjyOffset = container.clientHeight/2 - 40;
+            maxDisplacement = container.clientHeight/2.;
+            setTranslate(0, thumb); // move back to origin
+            // Remove the resize event listener after it has executed
+            window.removeEventListener('resize', afterOrientationChange);
+        };
+        window.addEventListener('resize', afterOrientationChange);
+    }
 
     if ( screen.orientation === undefined ) { // beautiful iOS not supporting the standard
         console.log(window.orientation)
         window.addEventListener('orientationchange', () => {
             console.log('iOS orientation changed');
-            var afterOrientationChange = function() { // NOTE: need an extra step to get the new dimensions _AFTER_ rotation
-                benjyOffset = container.clientHeight/2 - 40;
-                maxDisplacement = container.clientHeight/2.;
-                setTranslate(0, thumb); // move back to origin
-                // Remove the resize event listener after it has executed
-                window.removeEventListener('resize', afterOrientationChange);
-            };
-            window.addEventListener('resize', afterOrientationChange);
+            update_orientation();
         });
     }
     else {
         screen.orientation.addEventListener('change', () => {
-            // After orientationchange, add a one-time resize event
-            var afterOrientationChange = function() { // NOTE: need an extra step to get the new dimensions _AFTER_ rotation
-                benjyOffset = container.clientHeight/2 - 40;
-                maxDisplacement = container.clientHeight/2.;
-                setTranslate(0, thumb); // move back to origin
-                // Remove the resize event listener after it has executed
-                window.removeEventListener('resize', afterOrientationChange);
-            };
-            window.addEventListener('resize', afterOrientationChange);
+            console.log('non-iOS orientation changed')
+            update_orientation();
         });
     }
 
@@ -58,64 +53,62 @@ export function add_joystick(container) {
     thumb.addEventListener("mousemove", drag, false);
 
     function dragStart(e) {
-      if (e.type === "touchstart") {
-        if ( e.target.isSameNode(thumb) ) {
-            initialY = e.touches[0].clientY;
-        }
-      } else {
-        initialY = e.clientY;
-      }
+        // console.log('moving ', thumb);
+        if ( e.target === thumb ) {
+            if (e.type === "touchstart") {
+                initialY = e.touches[0].clientY;
+            } else {
+                initialY = e.clientY;
+            }
 
-      if (e.target === thumb) {
-        active = true;
-      }
+            active = true;
+        }
     }
 
     function dragEnd(e) {
-      active = false;
+        if ( e.target === thumb ) {
+            active = false;
 
-      setTranslate(0, thumb); // move back to origin
-      if ( val !== 0 ) {
-          container.dispatchEvent(stop_event);
-          val = 0;
-          finger = null;
-      }
+            setTranslate(0, thumb); // move back to origin
+
+            if ( val !== 0 ) {
+                container.dispatchEvent(stop_event);
+                val = 0;
+            }
+        }
     }
 
     function drag(e) {
-      if (active) {
+        if ( active && e.target === thumb ) {
 
-        e.preventDefault();
-
-        if (e.type === "touchmove") {
-            if ( e.target.isSameNode(thumb) ) {
+            e.preventDefault();
+            if (e.type === "touchmove") {
                 currentY = e.touches[0].clientY - initialY;
+            } else {
+                currentY = e.clientY - initialY;
             }
-        } else {
-          currentY = e.clientY - initialY;
-        }
 
-        currentY = Math.sign(currentY)*Math.min(Math.abs(currentY),maxDisplacement);
+            currentY = Math.sign(currentY)*Math.min(Math.abs(currentY),maxDisplacement);
 
-        setTranslate(currentY, thumb);
+            setTranslate(currentY, thumb);
 
-        if ( currentY/maxDisplacement > 0.5 && val !== -1) {
-            container.dispatchEvent(down_event);
-            val = -1;
+            if ( currentY/maxDisplacement > 0.5 && val !== -1) {
+                container.dispatchEvent(down_event);
+                val = -1;
+            }
+            else if ( currentY/maxDisplacement  < -0.5 && val !== 1) {
+                container.dispatchEvent(up_event);
+                val = 1;
+            }
+            else if (Math.abs(currentY/maxDisplacement) < 0.5 && val !== 0 ) {
+                container.dispatchEvent(stop_event);
+                val = 0;
+            }
         }
-        else if ( currentY/maxDisplacement  < -0.5 && val !== 1) {
-            container.dispatchEvent(up_event);
-            val = 1;
-        }
-        else if (Math.abs(currentY/maxDisplacement) < 0.5 && val !== 0 ) {
-            container.dispatchEvent(stop_event);
-            val = 0;
-        }
-      }
     }
 
     function setTranslate(yPos, el) {
-      el.style.transform = "translate(-50%, " + String(yPos + benjyOffset) + "px)";
+        el.style.transform = "translate(-50%, " + String(yPos + benjyOffset) + "px)";
     }
 
     let stop_event = new CustomEvent('move', { detail:0 , bubbles: true });
